fb40da1b4c88e792db1c5e6fe7634d8ffedd9779
tzResAndDev <iwasaki@typezero.jp>
2022-07-31 10:47:04 +0900

    edit page "HOME"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,97 +1,31 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-                <h1 className="text-6xl font-bold">
-                    Welcome to{' '}
-                    <a className="text-blue-600" href="https://nextjs.org">
-                        Next.js!
-                    </a>
-                </h1>
-
-                <p className="mt-3 text-2xl">
-                    Get started by editing{' '}
-                    <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-                        pages/index.tsx
-                    </code>
-                </p>
-
-                <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-                    <a
-                        href="https://nextjs.org/docs"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">
-                            Documentation &rarr;
-                        </h3>
-                        <p className="mt-4 text-xl">
-                            Find in-depth information about Next.js features and
-                            its API.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://nextjs.org/learn"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Learn about Next.js in an interactive course with
-                            quizzes!
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/canary/examples"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Discover and deploy boilerplate example Next.js
-                            projects.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Instantly deploy your Next.js site to a public URL
-                            with Vercel.
-                        </p>
-                    </a>
-                </div>
-            </main>
-
-            <footer className="flex items-center justify-center w-full h-24 border-t">
-                <a
-                    className="flex items-center justify-center gap-2"
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <Image
-                        src="/vercel.svg"
-                        alt="Vercel Logo"
-                        width={72}
-                        height={16}
-                    />
+const Home = () => {
+  return (
+    <div className="pt32 pb-12 md:pt-40 md:pb-20">
+      <div className="pb-12 text-center md:pb16">
+        <h1 className="leading-tight mb-4 text-7xl font-extrabold tracking-tighter md:text-8xl">
+          <span className="bg-gradient-to-r from-purple-600 to-blue-500 bg-clip-text text-transparent">
+            NFTission
+          </span>
+        </h1>
+        <div className="mx-auto max-w-3xl">
+          <p className="mb-8 text-xl">You can find favorite NFT Arts!</p>
+          <div className="flexjustify-center mt-5">
+            <div className="mt-3">
+              <Link href="/articles">
+                <a className="group inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 font-medium text-gray-900 hover:text-white focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800">
+                  <span className="rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+                    Find NFT Arts
+                  </span>
                 </a>
-            </footer>
+              </Link>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
